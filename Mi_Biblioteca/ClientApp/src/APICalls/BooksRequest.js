5e35ac3e83c3edfa67d51049502115b0d76319cd
Mehret Kulala <merutaye@gmail.com>
2019-01-28 20:09:18 -0600

    database setup complete

---
+++ Mi_Biblioteca/ClientApp/src/APICalls/BooksRequest.js
@@ -0,0 +1,16 @@
+﻿import axios from 'axios';
+
+const getBooks = () => {
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`https://www.goodreads.com/search.xml?key=AjJN7b1F1SFJRROEpMk2Gw&output=json`)
+            .then(res => {
+                resolve(res.data)
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
+}
+
+export default getBooks;
