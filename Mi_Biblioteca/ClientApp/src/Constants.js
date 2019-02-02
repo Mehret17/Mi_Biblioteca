9ced38ea0f2e9f8e48a74d4a01b5da60b65ecabe
Mehret Kulala <merutaye@gmail.com>
2019-02-02 10:27:25 -0600

    api works

--- Mi_Biblioteca/ClientApp/src/Constants.js
+++ Mi_Biblioteca/ClientApp/src/Constants.js
@@ -1,1 +1,5 @@
-﻿
+﻿const constants = {
+    goodReadsApiKey: 'aaEUhQEeP6hiV0xN1xa8Ag',
+};
+
+export default constants;
--- Mi_Biblioteca/ClientApp/src/components/Home/Home.js
+++ Mi_Biblioteca/ClientApp/src/components/Home/Home.js
@@ -1,12 +1,53 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
 import getBooks from '../../APICalls/BooksRequest';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
-class Home extends React {
+class Home extends React.Component {
+    state = {
+        books: [],
+        searchResult: '',
+    }
 
+    searchBooks = (e) => {
+        if (e.key === 'Enter') {
+            getBooks(this.state.searchResult)
+                .then(results => {
+                    this.setState({ books: results.items })
+                })
+                .catch(err => {
+                    console.error('error in searching books', err)
+                })
+        }
+    }
 
+    searchInput = (e) => {
+        this.setState({ searchResult: e.target.value });
+    }
     render() {
+        if (this.state.books) {
+            this.booksComponent = this.state.books.map(book => {
+                return (
+                    <div key={book.id}>
+                        <h4>{book.volumeInfo.title}
+                        </h4>
+                    </div>
+                )
+            })
+        }
         return (
+            <div className="Global">
+                <h2>Book Explorer!</h2>
+                <input
+                    type="text"
+                    placeholder="Search for a book"
+                    onChange={this.searchInput}
+                    onKeyPress={this.searchBooks}
+                />
+                {this.booksComponent}
+            </div>
 
-            )
+        )
     }
 }
+
+export default Home;
